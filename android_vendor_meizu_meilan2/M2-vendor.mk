dcde064d254f64b0dc097c06fa258c11ba1061ee
MASTERGUY <murtipipo@gmail.com>
2017-10-14 19:04:44 +0530

    message

---
+++ android_vendor_meizu_meilan2/M2-vendor.mk
@@ -0,0 +1,7 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/meilan2/overrides/bin,system/bin)
